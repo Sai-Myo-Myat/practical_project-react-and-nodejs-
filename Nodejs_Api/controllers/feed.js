aa9a7ba255dc31311909273e871c89474a38aa88
Sai Myo Myat <saimyomyat511@gmail.com>
2022-11-29 12:05:48 +0630

    fixing editing post

--- Nodejs_Api/controllers/feed.js
+++ Nodejs_Api/controllers/feed.js
@@ -1,5 +1,7 @@
 const PostModel = require("../models/post")
-const { validationResult } = require('express-validator')
+const { validationResult } = require('express-validator');
+const fs  = require('fs');
+const path = require("path");
 
 exports.getPosts = (req,res,next) => {
   PostModel.find()
@@ -103,4 +105,38 @@ exports.updatePost = (req,res,next) => {
     error.statusCode = 422;
     throw error;
   }
+
+  PostModel.findById(postId)
+    .then(post => {
+      if(!post) {
+        const error = new Error("Post not found.");
+        error.statusCode = 404;
+        throw error;
+      }
+      if(imageUrl !== post.imageUrl){
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.content = content;
+      post.imageUrl = imageUrl;
+      return post.save();
+    })
+    .then(result => {
+      res.status(200).json({
+        message: "post updated",
+        post: result
+      })
+    })  
+    .catch(err => {
+      if(!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err)
+    })
+
+}
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath);
 }
